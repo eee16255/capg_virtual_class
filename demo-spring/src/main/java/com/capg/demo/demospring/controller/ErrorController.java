28e6c10448ddc8e2a409bab766ac05487be3ccae
Yesumani jeevarathna <Yesumani jeevarathna@192.168.100.3>
2020-04-11 19:59:54 +0530

    Test

--- demo-spring/src/main/java/com/capg/demo/demospring/controller/ErrorController.java
+++ demo-spring/src/main/java/com/capg/demo/demospring/controller/ErrorController.java
@@ -6,14 +6,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-@RestControllerAdvice
+
+import com.capg.demo.demospring.exception.StudentNotFoundException;
+//@RestControllerAdvice
 public class ErrorController {
-	
-	
+	@ExceptionHandler(StudentNotFoundException.class)
+	@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "User Not Found" , code = HttpStatus.NOT_FOUND)
+	public void handleUserNotFoundException() {
 		
-		@ExceptionHandler(Exception.class)
-		@ResponseStatus(code=HttpStatus.NOT_FOUND,reason = "Student Not Found", value = HttpStatus.NOT_FOUND)
-		public void handleError() {
-			
-		}
+	}
 }
