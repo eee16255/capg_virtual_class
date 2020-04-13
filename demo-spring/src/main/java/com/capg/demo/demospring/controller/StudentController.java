c2b77b5845c013277a121e9e583c62c441b98bc5
Yesumani jeevarathna <Yesumani jeevarathna@192.168.100.4>
2020-04-13 11:49:17 +0530

    Test

--- demo-spring/src/main/java/com/capg/demo/demospring/controller/StudentController.java
+++ demo-spring/src/main/java/com/capg/demo/demospring/controller/StudentController.java
@@ -3,10 +3,13 @@ package com.capg.demo.demospring.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,31 +20,45 @@ import com.capg.demo.demospring.service.StudentDetailsService;
 @RestController
 @RequestMapping("/api")
 public class StudentController {
-	    @Autowired
-		StudentDetailsService  Service ;
+	@Autowired
+	StudentDetailsService service;
+	
+	@GetMapping("/students")
+	public ResponseEntity<List<Student>> getAllStudent(){
+		List<Student> allStudents=service.getListOfStudents();
 		
-		  @GetMapping("student")
-		public  List<Student>  getAllStudents()
-		{
-			  return Service.findall();
-					  
-		}
-		@GetMapping("student/{name}") 
-		public Student  findStudentsByName(@PathVariable("name") String studentName)
-		{
-			  return Service.findStudentByName(studentName);
-		}
+		return new ResponseEntity<List<Student>>(allStudents,HttpStatus.OK);
+	}
+	
+	@GetMapping("/students/id/{id}")
+	
+	public ResponseEntity<Student> getStudentById(@PathVariable("id") int studentId) {
 		
-		@DeleteMapping("student/{name}") 
-		public Student  deleteStudentByName(@PathVariable("name") String studentName)
-		{
-			  return Service.deleteStudentByName(studentName);
-		}
+		Student student= service.getStudent(studentId);
 		
-		@PostMapping("student") 
-		public Student  addStudentByName(@RequestBody Student  student)
-		{
-			  return Service.addStudent(student);
-		}
+		return new ResponseEntity<Student>(student,HttpStatus.OK);
+	}
+	
+	@PostMapping("/students")
+	public ResponseEntity<Student> addStudent(@RequestBody Student student){
+		service.addStudent(student);
+		return new ResponseEntity<Student>(student,HttpStatus.CREATED);
+	}
+
+	@PutMapping("/students")
+	public ResponseEntity<Student> updateStudent(@RequestBody Student student){
 		
+		
+		Student newStudentData=service.updateStudent(student);
+		
+		return new ResponseEntity<Student>(student,HttpStatus.OK);
+		
+	}
+
+	@DeleteMapping("/students/id/{id}")
+	public ResponseEntity<Student> deleteStudent(@PathVariable("id") int studentId) {
+		
+			return new ResponseEntity<Student>(HttpStatus.OK);
+		
+	}
 }
--- demo-spring/src/main/java/com/capg/demo/demospring/repo/StudentDetailsRepo.java
+++ demo-spring/src/main/java/com/capg/demo/demospring/repo/StudentDetailsRepo.java
@@ -1,11 +1,14 @@
 package com.capg.demo.demospring.repo;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+//import java.time.LocalDate;
+//import java.util.ArrayList;
+//import java.util.Collection;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
@@ -14,38 +17,15 @@ import com.capg.demo.demospring.model.Student;
 @Repository
 public class StudentDetailsRepo {
 	
-	
-
-		Map<Integer,Student >  map = new HashMap<>();
+		@PersistenceContext
+		EntityManager em;
 		
-		public StudentDetailsRepo() {
-			
-			Student  e1= new Student(100,"Rebeca",LocalDate.of(1999, 04, 16));
-			Student  e2= new Student(101,"chitti",LocalDate.of(1998, 02, 24));
-			map.put(100, e1);
-			map.put(101, e2);
+		public Student addStudent(Student student) {
+			em.persist(student);
+			return student;
 		}
 		
-		public List<Student> findall() {
-			Collection<Student> values = map.values();
-			List<Student>   list = new ArrayList<>(values);
-			return list;
-		}
-
-		public Student   findStudentByName(String studentName)
-		{
-			return map.get(studentName);
-		}
-		
-		public  Student deleteStudentByName(String studentName)
-		{
-			return  map.remove(studentName);
-		}
-		
-		public Student  addStudent(Student student)
-		{
-			map.put(student.getStudentId(), student);
-			
-			return student ;
+		public  Student getUser(int studentId) {
+			return em.find(Student.class, studentId);
 		}
 }
