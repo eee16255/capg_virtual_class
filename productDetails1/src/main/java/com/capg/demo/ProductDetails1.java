6acb18bde2301cfe49bfa53e1ec0507ed93e23f0
Yesumani jeevarathna <Yesumani jeevarathna@192.168.100.6>
2020-04-18 15:03:44 +0530

    spring lab 3

--- productDetails1/src/main/java/com/capg/demo/ProductDetails1.java
+++ productDetails1/src/main/java/com/capg/demo/ProductDetails1.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductDetails_lab3Application {
+public class ProductDetails1 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductDetails_lab3Application.class, args);
+		SpringApplication.run(ProductDetails1.class, args);
 	}
 
 }
